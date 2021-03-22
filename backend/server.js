91bac00caa351538c5d07f2f32d89bc83584934d
GeronimoJohn <johnageronimo1@gmail.com>
2021-03-22 15:33:30 +1300

    Created sample products and added to mongodb, created api routes for products too

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const data = require("./data");
 const productRouter = require("./routers/productRouter");
 const userRouter = require("./routers/userRouter");
 
@@ -18,24 +17,11 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
 // user router
 app.use("/api/users", userRouter);
 
 // products router
 app.use("/api/products", productRouter);
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product not Found" });
-  }
-});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
