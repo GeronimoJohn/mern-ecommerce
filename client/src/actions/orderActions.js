059fef982750eeb91e7edfd5a7cc19bf472b4294
GeronimoJohn <johnageronimo1@gmail.com>
2021-04-04 12:40:37 +1200

    Started creating the order screen page

--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -2,6 +2,8 @@ import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_REQUEST,
 } from "../constants/orderConstants";
 import Axios from "axios";
 import { CART_EMPTY } from "../constants/cartConstants";
@@ -28,5 +30,25 @@ export const createOrder = (order) => async (dispatch, getState) => {
           ? error.response.data.message
           : error.message,
     });
+  }
+};
+
+export const detailsOrder = (orderId) => async (dispatch, getState) => {
+  dispatch({ type: ORDER_DETAILS_REQUEST, payload: orderId });
+  const {
+    userSignin: { userInfo },
+  } = getState();
+  try {
+    const { data } = await Axios.get(`/api/orders/${orderId}`, {
+      headers: {
+        Authorization: `Bearer ${userInfo.data.token}`,
+      },
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: ORDER_DETAILS_FAIL, payload: message });
   }
 };
